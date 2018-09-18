0c7d0020ab12a541552631a3ab1d50d7d2e5b0f8
thgoexpt <thomaseaton63@gmail.com>
2018-09-18 11:43:22 +0700

    Implements mux using gorilla

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+var t = template.Must(template.ParseGlob("template/*"))
+
 func main() {
-	fmt.Println("Welcome to SOLID company")
+	r := mux.NewRouter()
+	fs := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
+
+	r.PathPrefix("/static/").Handler(fs)
+	r.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", r)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	t.ExecuteTemplate(w, "hello.html", nil)
 }
