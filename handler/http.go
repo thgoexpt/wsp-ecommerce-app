7e150779eacba72dc71c7c2688b50e33e62302ee
thgoexpt <thomaseaton63@gmail.com>
2018-10-24 02:12:13 +0700

    Ignore https case

--- handler/http.go
+++ handler/http.go
@@ -25,20 +25,22 @@ func RedirectToHTTPS(w http.ResponseWriter, r *http.Request) {
 }
 
 func RedirectToHTTPSMiddleware(w http.ResponseWriter, r *http.Request, v *middleware.ValueMap) {
-	host, _, err := net.SplitHostPort(r.Host)
-	if err != nil {
-		host = r.Host
-	}
-	port := ":443"
-	if env.GetEnv() != "PRODUCTION" {
-		port = ":4433"
-	}
-	target := "https://" + host + port + r.URL.Path
-	if len(r.URL.RawQuery) > 0 {
-		target += "?" + r.URL.RawQuery
-	}
+	host, p, err := net.SplitHostPort(r.Host)
+	if p != "433" {
+		if err != nil {
+			host = r.Host
+		}
+		port := ":443"
+		if env.GetEnv() != "PRODUCTION" {
+			port = ":4433"
+		}
+		target := "https://" + host + port + r.URL.Path
+		if len(r.URL.RawQuery) > 0 {
+			target += "?" + r.URL.RawQuery
+		}
 
-	http.Redirect(w, r, target, http.StatusPermanentRedirect)
+		http.Redirect(w, r, target, http.StatusPermanentRedirect)
 
+	}
 	v.Set("next", true)
 }
