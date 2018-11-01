f083c75b869c1278846f5c391b51c009ce27ed15
thgoexpt <thomaseaton63@gmail.com>
2018-11-01 23:50:34 +0700

    init MakeSalesHistory

--- model/dbmodel/history.go
+++ model/dbmodel/history.go
@@ -1,6 +1,7 @@
 package dbmodel
 
 import (
+	"errors"
 	"github.com/globalsign/mgo/bson"
 	"time"
 )
@@ -11,4 +12,25 @@ type SalesHistoryModel struct {
 	Meats map[bson.ObjectId]int `bson:"meats"`
 	Price float64 `bson:"price"`
 	TrackingNumber string `bson:"tracking_number"`
+}
+
+func MakeSalesHistory(time time.Time, meats map[Meat]int, price float64, trackingNum string) (SalesHistoryModel, error) {
+	meatsID := map[bson.ObjectId]int{}
+
+	for k,v := range meats {
+		if k.ID == "" {
+			return SalesHistoryModel{}, errors.New("no meat id: please get id from database")
+		}
+
+		meatsID[k.ID] = v
+	}
+
+	sh := SalesHistoryModel{
+		Time:time,
+		Meats:meatsID,
+		Price:price,
+		TrackingNumber:trackingNum,
+	}
+
+	return sh, nil
 }
